d876ea27c66340e0a5fb7ac6b96a7e32c3c72cad
kukjin <thater114@gmail.com>
2024-02-12 19:37:20 +0900

    EP04 commit

---
+++ pythonbasic/EP04_datatype_structure.py
@@ -0,0 +1,48 @@
+
+
+# 정수
+age = 20
+print(age)
+print(type(age))
+
+# 실수
+weight = 80.5
+print(weight)
+print(type(weight))
+
+# 문자열
+greeting = "Hello World!"
+print(greeting)
+print(type(greeting))
+
+# 불리언 boolean
+is_adult = True
+print(is_adult)
+print(type(is_adult))
+
+# ------------------------
+# 파이썬의 자료구조
+
+# 리스트
+fruits = ['사과',"바나나","딸기"]
+print(fruits)
+print(type(fruits))
+
+# 튜플
+location = ("37.123","126.957")
+print(location)
+print(type(location))
+
+
+# 딕셔너리
+student_scores={'철수':90,'영희':85,'민수':95}
+print(student_scores)
+print(type(student_scores))
+
+# 세트
+colors = {'red','red','blue','green','blue','yellow','black'}
+print(colors)
+print(type(colors))
+
+
+
